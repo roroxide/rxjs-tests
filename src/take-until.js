cd24c85c9ec77254631e54fcc0662f0faa996da1
Roham R. Tehrani <r.tehrani@rayanmail.com>
2024-03-13 16:52:25 +0330

    initialize project with takeuntil

---
+++ src/take-until.js
@@ -0,0 +1,6 @@
+import { interval, fromEvent, takeUntil } from 'rxjs';
+
+const source = interval(1000);
+const clicks = fromEvent(document, 'click');
+const result = source.pipe(takeUntil(clicks));
+result.subscribe(x => console.log(x));
